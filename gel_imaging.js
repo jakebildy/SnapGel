864fa56d5a09a545afc01b897a4aaf4666028eac
Jacob Bildy <jbildy@uwo.ca>
2019-04-17 05:17:08 -0700

    :bug: video now works

--- gel_imaging.js
+++ gel_imaging.js
@@ -1,32 +1,8 @@
-navigator.getUserMedia ||
-(navigator.getUserMedia = navigator.mozGetUserMedia ||
-    navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+const constraints = {
+    video: true
+};
 
-if (navigator.getUserMedia) {
-    navigator.getUserMedia({
-        video: true,
-        audio: false
-    }, onSuccess, onError);
-} else {
-    alert('getUserMedia is not supported! Try a different browser');
-}
+const video = document.querySelector('video');
 
-
-
-function onSuccess() {
-    var video = document.getElementById('webcam');
-    var videoSource;
-
-    if (window.webkitURL) {
-        videoSource = window.webkitURL.createObjectURL(stream);
-    } else {
-        videoSource = stream;
-    }
-
-    video.autoplay = true;
-    video.src = videoSource;
-}
-
-function onError() {
-    alert('Issue enabling streams - try allowing access');
-}
+navigator.mediaDevices.getUserMedia(constraints).
+then((stream) => {video.srcObject = stream});
