2ac011aca1d55af12c8c5bd8ca66fe257da0edf4
Jacob Bildy <jbildy@uwo.ca>
2019-05-01 09:45:47 -0700

    :sparkles: rgb to hsl

--- gel_imaging.js
+++ gel_imaging.js
@@ -63,7 +63,7 @@ function renderColorspace(data) {
 
         data[i + 2] = 0; // b
 
-        if (data[i] < 100) {
+        if (rgb2hue(data[i], data[i+1], data[i+2]) > rgb2hue(31,255,0)) {
             data[i + 1] = 255; // g
             data[i] = 255;
             data[i+2] = 255;
@@ -77,6 +77,41 @@ function renderColorspace(data) {
 
     }
     return data;
+}
+
+function rgb2hue(r, g, b) {
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    var c   = max - min;
+    var hue;
+    if (c == 0) {
+        hue = 0;
+    } else {
+        switch(max) {
+            case r:
+                var segment = (g - b) / c;
+                var shift   = 0 / 60;       // R° / (360° / hex sides)
+                if (segment < 0) {          // hue > 180, full rotation
+                    shift = 360 / 60;         // R° / (360° / hex sides)
+                }
+                hue = segment + shift;
+                break;
+            case g:
+                var segment = (b - r) / c;
+                var shift   = 120 / 60;     // G° / (360° / hex sides)
+                hue = segment + shift;
+                break;
+            case b:
+                var segment = (r - g) / c;
+                var shift   = 240 / 60;     // B° / (360° / hex sides)
+                hue = segment + shift;
+                break;
+        }
+    }
+    return hue * 60; // hue is in [0,6], scale it up
 }
 
 function renderBlackWhite(data) {
